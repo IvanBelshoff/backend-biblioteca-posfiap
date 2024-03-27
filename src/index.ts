8c4aa6f9486701a09a80308994bb3cbd207dae6b
IvanBelshoff <ivanbelshoff@gmail.com>
2024-03-26 22:22:24 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import 'reflect-metadata';
+
+import { server } from './server/Server';
+import { AppDataSource } from './server/database';
+import { TypeORMError } from 'typeorm';
+
+AppDataSource.initialize().then(async () => {
+
+    // eslint-disable-next-line quotes
+    console.log(`\nBanco de dados conectado\n`);
+
+    server.listen(process.env.PORT, async () => {
+        console.log(`Serssvidor rodando no endereço: http://${process.env.HOST}:${process.env.PORT}\n`);
+    });
+
+}).catch((error) => {
+    console.log(error as TypeORMError);
+
+    if (error.code == String('3D000')) {
+        console.log(error);
+    }
+});
